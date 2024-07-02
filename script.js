6f586ecb53c8a407b39ebab478e5b164d096b217
Musa Tahir <musaasimtahir@gmail.com>
2024-07-02 00:32:28 -0700

    feat: Add control panel with a slider that lets user reset grid dimensions

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
+const gridContainer = document.querySelector(".grid-container");
+let sizeDisplay = document.querySelector(".grid-size");
+let inputSlider = document.querySelector("input");
+let applyButton = document.querySelector(".apply");
+let gridSize = 16;
+generateGrid(gridSize)
+
+
+
 function generateGrid(size) {
-    const gridContainer = document.querySelector(".grid-container");
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement('div');
         cell.classList.add("grid-cell");
@@ -10,12 +18,29 @@ function generateGrid(size) {
     }
 }
 
-generateGrid(16);
-const grid = document.querySelector('.grid-container')
+
+function resetGrid() {
+    gridContainer.innerHTML = '';
+    generateGrid(gridSize);
+
+}
+
 
 // using event delegation to handle all child nodes simultaneously
-grid.addEventListener('mouseover', (e) => {
+gridContainer.addEventListener('mouseover', (e) => {
     if (e.target.matches('.grid-cell')) {
         e.target.style.backgroundColor = 'black';
     }
 })
+
+inputSlider.addEventListener('input', (e) => {
+    gridSize = e.target.value;
+    sizeDisplay.innerHTML = `${gridSize}x${gridSize}`;
+})
+
+applyButton.addEventListener('click', () => {
+    resetGrid()
+})
+
+
+
